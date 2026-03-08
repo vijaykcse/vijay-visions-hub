3cb0a9f301ddce616cc4dafa9ad735860e613cf3
vijaykcse <vijaykcode@gmail.com>
2026-03-08 19:48:24 +0530

    Fix 404 and blank screen using HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+// 1. Change BrowserRouter to HashRouter
+import { HashRouter } from 'react-router-dom' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* 2. Wrap the App in HashRouter */}
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
